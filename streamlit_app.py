601880ea16dd69d246afba3a1fb79e3e599ef7a8
John R. Dingler <166140606+john-dingler@users.noreply.github.com>
2025-03-21 13:45:28 -0400

    Update streamlit_app.py
    Exposed JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
     time_to_insert = st. button ('Submit Order')
     
     smoothiefroot_response = requests.get ("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st. text (smoothiefroot_response)
+    st. text (smoothiefroot_response.json())
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
