a046b610637ae8267501472abf9c7f45854e8940
John R. Dingler <166140606+john-dingler@users.noreply.github.com>
2025-03-21 14:59:26 -0400

    Update streamlit_app.py
    Debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-  # Import python packages
- import streamlit as st
- from snowflake.snowpark.functions import col
- import requests
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
 
 st.success("Snowpark is working!")
 # helpful_links = [
