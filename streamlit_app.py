ee85fbdbbaaae985c156cbd1e4db51c673bf2cec
John R. Dingler <166140606+john-dingler@users.noreply.github.com>
2025-03-21 19:18:45 -0400

    Update streamlit_app.py
    Changed option name from FRUIT_NAME to SEARCH_ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+RCH_ON# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -22,7 +22,7 @@ cnx = st. connection ("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-my_dataframe = session. table ("smoothies.public.fruit_options").select (col ('FRUIT_NAME'))
+my_dataframe = session. table ("smoothies.public.fruit_options").select (col ('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect( 'Choose up to 5 ingredients:', my_dataframe, max_selections=5)
